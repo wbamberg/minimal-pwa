911d4d796759ebb3c2c33c01ab9a8a3690807f12
wbamberg <will@bootbonnet.ca>
2024-06-24 10:50:11 -0700

    Also maybe fix sw

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-navigator.serviceWorker.register("/sw.js");
+navigator.serviceWorker.register("sw.js");
 
 let installPrompt = null;
 const installButton = document.querySelector("#install");
@@ -8,22 +8,22 @@ window.addEventListener("beforeinstallprompt", enableAppInstallPrompt);
 window.addEventListener("appinstalled", disableInAppInstallPrompt);
 
 function enableAppInstallPrompt(event) {
-	event.preventDefault();
-	installPrompt = event;
-	installButton.removeAttribute("hidden");
-	installButton.addEventListener("click", inAppInstallPrompt);
+  event.preventDefault();
+  installPrompt = event;
+  installButton.removeAttribute("hidden");
+  installButton.addEventListener("click", inAppInstallPrompt);
 }
 
 function disableInAppInstallPrompt() {
-	installPrompt = null;
-	installButton.setAttribute("hidden", "");
-	installButton.removeEventListener("click", inAppInstallPrompt);
+  installPrompt = null;
+  installButton.setAttribute("hidden", "");
+  installButton.removeEventListener("click", inAppInstallPrompt);
 }
 
 async function inAppInstallPrompt() {
-	if (!installPrompt) return;
+  if (!installPrompt) return;
 
-	const result = await installPrompt.prompt();
-	console.log(`Install prompt was: ${result.outcome}`);
-	disableInAppInstallPrompt();
+  const result = await installPrompt.prompt();
+  console.log(`Install prompt was: ${result.outcome}`);
+  disableInAppInstallPrompt();
 }
